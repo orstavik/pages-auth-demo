84333c6bcd454987f39f3619c564cfdf9e393d9e
intertext.no <orstavik77@gmail.com>
2023-01-10 12:20:57 +0100

    testing logging

--- functions/hello.js
+++ functions/hello.js
@@ -1,11 +1,11 @@
 export async function onRequest({request, env}){
+  console.log("ivar", {hello: "sunshine"}, 42);
   const res = `
   <pre>
   hello sunshine
 GITHUB_CLIENT_ID=${env.GITHUB_CLIENT_ID}
 GITHUB_CLIENT_SECRET=${env.GITHUB_CLIENT_SECRET}
 GITHUB_REDIRECT=${env.GITHUB_REDIRECT}
-</pre>
-`;
+</pre>`;
   return new Response(res);
 }
