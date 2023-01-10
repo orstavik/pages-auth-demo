06105d68188565d149258fa57200880b47909330
intertext.no <orstavik77@gmail.com>
2023-01-10 11:00:05 +0100

    adding some html and routes

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,4 @@
-export async function onRequest(context){
+export async function onRequest({request}){
+  console.log("bob", Object.fromEntries(request.headers));
   return new Response("hello sunshine in Lviv and Tønsberg!");
 }
