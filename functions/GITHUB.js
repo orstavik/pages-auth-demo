0ee4cc54a033dc7cbffe31cded026979fab863fa
intertext.no <orstavik77@gmail.com>
2023-01-11 11:41:13 +0100

    GITHUB resources as a library.

---
+++ functions/GITHUB.js
@@ -0,0 +1,32 @@
+export class GITHUB {
+
+  //https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#1-request-a-users-github-identity
+  static loginLink({GITHUB_REDIRECT, GITHUB_CLIENT_ID}, state) {
+    const url = new URL("https://github.com/login/oauth/authorize");
+    url.searchParams.set("client_id", GITHUB_CLIENT_ID);
+    url.searchParams.set("redirect_uri", GITHUB_REDIRECT);
+    url.searchParams.set("scope", "user");
+    url.searchParams.set("state", state);
+    // url.searchParams.set("login", previous username);
+    // url.searchParams.set("allow_signup", true); true by default
+    return url.href;
+  }
+
+  static fetchAccessToken(code, client_id, redirect_uri, client_secret, state) {
+    return fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+      body: JSON.stringify({code, client_id, client_secret, redirect_uri, state})
+    });
+  }
+
+  static fetchUserData(accessToken) {
+    return fetch('https://api.github.com/user', {
+      headers: {
+        'Authorization': 'token ' + accessToken,
+        'User-Agent': 'Mozilla/5.0',
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    });
+  }
+}
--- functions/login.js
+++ functions/login.js
@@ -3,28 +3,14 @@ function randomString(length) {
   return Array.from(iv).map(b => ('00' + b.toString(16)).slice(-2)).join('');
 }
 
-function githubLoginLink({GITHUB_REDIRECT, GITHUB_CLIENT_ID}, state) {
-  const url = new URL("https://github.com/login/oauth/authorize");
-  url.searchParams.set("client_id", GITHUB_CLIENT_ID);
-  url.searchParams.set("redirect_uri", GITHUB_REDIRECT);
-  url.searchParams.set("scope", "user");
-  url.searchParams.set("state", state);
-  // url.searchParams.set("login", previous username);
-  // url.searchParams.set("allow_signup", true); true by default
-  return url.href;
+function makeState(key) {
+  //todo the state should be a timestamp, and this timestamp should be encrypted using an algorithm.
+  return randomString(12);
 }
 
-//https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#1-request-a-users-github-identity
+import {GITHUB} from "./GITHUB";
 
 export async function onRequest({request, env}) {
-  const state = randomString(12);
-  //todo the state can be added to a KV, so that the callback can be checked for fakes.
-  const href = githubLoginLink(env, state);
-  return new Response(`<a href="${href}">login here</a>`, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
-  // return Response.redirect(href);
+  const href = GITHUB.loginLink(env, makeState(env.STATESECRET));
+  return Response.redirect(href);
 }
