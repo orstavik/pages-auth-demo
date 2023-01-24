a9d123cac2e9cf6aa2efb25921de6e08bfd768b1
intertext.no <orstavik77@gmail.com>
2023-01-24 15:48:55 +0100

    babystep

--- functions/fromGithub.js
+++ functions/fromGithub.js
@@ -5,11 +5,15 @@ function bakeCookie(name, value, domain, ttl) {
   return `${name}=${value}; Domain=${domain}; SameSite=LAX; Max-Age=${ttl}; path=/; secure; httpOnly`;
 }
 
-export async function onRequest(context) {
+export async function onRequest({request, env}) {
   const {
-    request,
-    env: {GITHUB_CLIENT_ID, GITHUB_REDIRECT, GITHUB_CLIENT_SECRET, STATE_SECRET, SESSION_SECRET, SESSION_TTL}
-  } = context;
+    GITHUB_CLIENT_ID,
+    GITHUB_REDIRECT,
+    GITHUB_CLIENT_SECRET,
+    STATE_SECRET,
+    SESSION_SECRET,
+    SESSION_TTL
+  } = env;
   const params = new URL(request.url).searchParams;
   const code = params.get('code');
   const state = params.get('state');
@@ -21,18 +25,17 @@ export async function onRequest(context) {
 
   const responseAccessToken = await GITHUB.fetchAccessToken(code, GITHUB_CLIENT_ID, GITHUB_REDIRECT, GITHUB_CLIENT_SECRET, state);
   const data = await responseAccessToken.json();
-  const responseUserData = await GITHUB.fetchUserData(data['access_token']);
+  const responseUserData = await GITHUB.fetchUserData(data.access_token);
   const userData = await responseUserData.json();
 
-  const cookiePayload = {
+  const base64cookieToken = await encodeBase64Token(SESSION_SECRET, {
     user: userData.login + "@github",
     ttl: SESSION_TTL,
     rights: "edit,admin", //todo this we need to get from the environment variables
     ip: request.headers.get("CF-Connecting-IP")
-  };
-  const base64cookieToken = await encodeBase64Token(SESSION_SECRET, cookiePayload);
+  });
   const response = Response.redirect(new URL("/", request.url));
   response.headers.set("Set-Cookie",
-    bakeCookie("id", base64cookieToken, new URL(context.request.url).hostname, SESSION_TTL));
+    bakeCookie("id", base64cookieToken, new URL(request.url).hostname, SESSION_TTL));
   return response;
 }
