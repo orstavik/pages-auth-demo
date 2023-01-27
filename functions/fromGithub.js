5035c62763c68ad2ef82b20af7a695999fbeff3a
intertext.no <orstavik77@gmail.com>
2023-01-27 21:47:05 +0100

    works

--- functions/fromGithub.js
+++ functions/fromGithub.js
@@ -1,26 +1,42 @@
 import {GITHUB, bakeCookie} from "./AUTH";
 import {Base64Token} from "./AES-GCM";
+import {appContext} from "./APP";
 
-
-export async function onRequest({request, env}) {
-  const {
-    GITHUB_CLIENT_ID,
-    GITHUB_REDIRECT,
-    GITHUB_CLIENT_SECRET,
-    STATE_SECRET,
-    SESSION_SECRET,
-    SESSION_TTL
-  } = env;
-  const params = new URL(request.url).searchParams;
-  const code = params.get('code');
-  const state = params.get('state');
-  try {
-    const emptyBase64Token = await Base64Token.decode(STATE_SECRET, state);
-  } catch (err) {
-    return new Response('state error', {status: 500});
+const whitelist = {
+  timeStamp: 1,
+  request: {
+    url: {
+      searchParams: {
+        state: 1,
+        code: 1
+      }
+    }
+  },
+  env: {
+    rights: 1
   }
+};
+let proxy;
 
-  const responseAccessToken = await GITHUB.fetchAccessToken(code, GITHUB_CLIENT_ID, GITHUB_REDIRECT, GITHUB_CLIENT_SECRET, state);
+export async function onRequest(context) {
+  let state = (proxy ??= appContext(context.env.SESSION_SECRET)).filter(whitelist, context);
+  state instanceof Promise && (state = await state);
+  const {
+    request, env: {
+      GITHUB_CLIENT_ID,
+      GITHUB_REDIRECT,
+      GITHUB_CLIENT_SECRET,
+      STATE_SECRET,
+      SESSION_SECRET,
+      SESSION_TTL
+    }
+  } = context;
+  const {code, state: stateParam} = state.request.url.searchParams;
+    const emptyBase64Token = await Base64Token.decode(STATE_SECRET, stateParam);
+  if(!emptyBase64Token)
+    return new Response('state error', {status: 500});
+
+  const responseAccessToken = await GITHUB.fetchAccessToken(code, GITHUB_CLIENT_ID, GITHUB_REDIRECT, GITHUB_CLIENT_SECRET, stateParam);
   const data = await responseAccessToken.json();
   const responseUserData = await GITHUB.fetchUserData(data.access_token);
   const userData = await responseUserData.json();
