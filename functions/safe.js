06f91ef1d98612b7617eed2b85a4d1d5938f3168
intertext.no <orstavik77@gmail.com>
2023-01-27 21:36:16 +0100

    works

--- functions/safe.js
+++ functions/safe.js
@@ -11,13 +11,37 @@ const whitelist = {
   }
 };
 
+const redirectResponse = [
+  "Unauthorized",
+  {
+    status: 302,
+    headers: {
+      Location: "/login"
+    }
+  }
+];
+
 let proxy;
 
+function redirectWhenNoSession(session) {
+  if (session)         //and the session has the rights to this base
+    return;
+  return redirectResponse;
+}
+
 export async function onRequest(context) {
+  //1. normalize the incoming request and environment. This includes decrypting cookies.
   let state = (proxy ??= appContext(context.env.SESSION_SECRET)).filter(whitelist, context);
   state instanceof Promise && (state = await state);
-  const session = state.request.headers.cookie.id;
-  if (!session)
-    return Response.redirect(new URL("/login", state.request.url));
-  return new Response("Safe, cookie content: " + JSON.stringify(session, null, 2), {status: 200});
+
+  //2. check that there is a valid session.
+  state.response = redirectWhenNoSession(state.request.headers.cookie.id);
+
+  //3. if the state.response is still open for business, then add the body
+  state.response ??= [JSON.stringify(state, null, 2)];
+
+  //4. save the state
+
+  //5. convert the state to a response
+  return new Response(...state.response);
 }
