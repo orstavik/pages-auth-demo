6107fc46ae29e8e9c60d936bf78b9b47d77f1348
intertext.no <orstavik77@gmail.com>
2023-01-30 13:52:39 +0100

    babystep

--- functions/fromGoogle.js
+++ functions/fromGoogle.js
@@ -1,35 +1,55 @@
 import {bakeCookie, GOOGLE} from "./AUTH";
 import {Base64Token} from "./AES-GCM";
+import {appContext2} from "./APP";
+
+const whitelist = {
+  timeStamp: 1,
+  request: {
+    headers: {
+      "CF-Connecting-IP": 1
+    },
+    url: {
+      href: 1,
+      hostname: 1,
+      searchParams: {
+        code: 1,
+        state: 1
+      }
+    }
+  },
+  env: {
+    rights: 1
+  }
+}
+
+let proxy;
 
 export async function onRequest(context) {
-  const {request,
-    env: {
-      GOOGLE_CLIENT_ID,
-      GOOGLE_CODE_LINK,
-      GOOGLE_REDIRECT,
-      GOOGLE_CLIENT_SECRET,
-      STATE_SECRET,
-      SESSION_SECRET,
-      SESSION_TTL
-    }
-  } = context;
-  const params = new URL(request.url).searchParams;
-  const code = params.get('code');
-  const state = params.get('state');
-  const emptyBase64Token = await Base64Token.decode(STATE_SECRET, state);
-  if (!emptyBase64Token)
+  let state = (proxy ??= appContext2(context.env)).filter(whitelist, context);
+  state instanceof Promise && (state = await state);
+
+  const {
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CODE_LINK,
+    GOOGLE_REDIRECT,
+    GOOGLE_CLIENT_SECRET,
+    SESSION_SECRET,
+    SESSION_TTL
+  } = context.env;
+  const {code, state: stateParam} = state.request.url.searchParams;
+  if (!stateParam)
     return new Response('state error', {status: 500});
 
   const payload = await GOOGLE.getUserData(code, GOOGLE_CODE_LINK, GOOGLE_CLIENT_ID, GOOGLE_REDIRECT, GOOGLE_CLIENT_SECRET, 'authorization_code');
 
   const base64cookieToken = await Base64Token.encode(SESSION_SECRET, {
     user: payload.email,
-    rights: "edit,admin", //todo this we need to get from the environment variables
-    ip: request.headers.get("CF-Connecting-IP"),
+    rights: state.env.rights[payload.email],
+    ip: state.request.headers["CF-Connecting-IP"],
     ttl: SESSION_TTL
   });
-  const response = Response.redirect(new URL("/", request.url));
+  const response = Response.redirect(new URL("/", state.request.url.href));
   response.headers.set("Set-Cookie",
-    bakeCookie("id", base64cookieToken, new URL(request.url).hostname, SESSION_TTL));
+    bakeCookie("id", base64cookieToken, state.request.url.hostname, SESSION_TTL));
   return response;
 }
