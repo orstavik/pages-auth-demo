985180dab5bb3138058983abf1915da339ba89eb
intertext.no <orstavik77@gmail.com>
2023-01-30 11:58:42 +0100

    babystep

--- functions/basics/cookie.js
+++ functions/basics/cookie.js
@@ -1,6 +1,6 @@
 export function readCookies(request) {
   const cStr = request.headers.get("Cookie");
-  return cStr ? Object.fromEntries(cStr.split(";").map(i => i.split("=").map(s => s.trim()))): {};
+  return cStr ? Object.fromEntries(cStr.split(";").map(p => p.split(/=(.+)/).map(s => s?.trim()))) : {};
 }
 
 //1. convert incoming cookies to a dictionary
@@ -8,11 +8,13 @@ export function readCookies(request) {
 //3. respond with cookies
 export async function onRequest({request}) {
   const cookies = readCookies(request);
-  const response = new Response(JSON.stringify(cookies, null, 2));
-  response.headers.set("Set-Cookie", [
-    `hello=world`,                                               //a. add hello world
-    `hello=sunshine${new Date().getTime()};`,                    //b. overwrite hello=world with hello=sunshine
-    `sunshine=hello;` + (Math.random() > .5 ? "max-age=0" : "")  //c. randomly add/remove sunshine=hello
-  ]);
-  return response;
+  return new Response(JSON.stringify(cookies, null, 2), {
+    headers: {
+      "Set-Cookie": [
+        `hello=world`,                            //a. add hello world
+        `hello=sunshine${new Date().getTime()};`, //b. overwrite hello=world with hello=sunshine
+        `sunshine=hello;` + (Math.random() > .5 ? "max-age=0" : "") //c. add/remove sunshine=hello
+      ]
+    }
+  });
 }
