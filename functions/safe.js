b9074c6d3c85f82bbfdf0ab1700edda3f81f1cc4
intertext.no <orstavik77@gmail.com>
2023-01-31 21:54:16 +0100

    babystep

--- functions/safe.js
+++ functions/safe.js
@@ -1,18 +1,4 @@
-import {appContext} from "./APP";
-
-const whitelist = {
-  request: {
-    url: {
-      href: 1,
-      pathname: 1
-    },
-    headers: {
-      cookie: {
-        id: 1
-      }
-    }
-  }
-};
+import {appContext, appContext3} from "./APP";
 
 const redirectResponse = [
   "Unauthorized",
@@ -24,26 +10,40 @@ const redirectResponse = [
   }
 ];
 
-let proxy;
-
 function redirectWhenNoSession(session, location) {
   if (session?.rights.indexOf(location.split("/")[1]) < 0)
     return redirectResponse;
 }
 
+const whitelist2 = {
+  now: {
+    ip: "request.headers.cf-connecting-ip",
+    iat: "timeStamp",
+    ttl: "env.SESSION_TTL"
+  },
+  url: "request.url.href",
+  pathname: "request.url.pathname",
+  session: "request.headers.cookie.id",
+  state: "request.url.searchParams.state",
+  code: "request.url.searchParams.code",
+  rights: "env.rights"
+};
+
+let proxy;
+
 export async function onRequest(context) {
-  //1. normalize the incoming request and environment. This includes decrypting cookies.
-  let state = (proxy ??= appContext(context.env.SESSION_SECRET)).filter(whitelist, context);
+  proxy ??= appContext3(context.env, whitelist2);
+  let state = proxy.filter(context);
   state instanceof Promise && (state = await state);
 
   //2. check that there is a valid session.
-  state.response = redirectWhenNoSession(state.request.headers.cookie.id, state.request.url.pathname);
+  state.response = redirectWhenNoSession(state.session, state.pathname);
 
   //3. if the state.response is still open for business, then add the body
   state.response ??= [JSON.stringify(state, null, 2)];
 
   //4. save the state
 
-  //5. convert the state to a response
+  //5. convert the state to a response+
   return new Response(...state.response);
 }
