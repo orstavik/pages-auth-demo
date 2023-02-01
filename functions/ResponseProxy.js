0af58887efe8d0cf7d9420469d4a77b8365fd933
intertext.no <orstavik77@gmail.com>
2023-02-01 22:25:09 +0100

    some cleaning

--- functions/ResponseProxy.js
+++ functions/ResponseProxy.js
@@ -15,21 +15,26 @@ function getParents(obj, path) {
   return path.map(p => obj = obj[p]);
 }
 
-export function getValues(filter, state) {
-  const res = {};
-  //normalize the sortedFilters
-  const sortedFilters = Object.entries(filter)
+function normalizeFilter(filter) {
+  return Object.entries(filter)
     .sort((a, b) => b[0].length - a[0].length)
     .map(([k, v]) => {
       const resPath = k.split(".");
       const prop = resPath.pop();
-      return [[resPath, prop], v instanceof Array ? [v[0].split("."), v[1]] : v instanceof Function ? [, v] : [v.split(".")]];
+      const val = v instanceof Array ? [v[0].split("."), v[1]] : v instanceof Function ? [, v] : [v.split(".")];
+      return [resPath, prop, ...val];
     });
+}
+
+export function getValues(filter, state) {
+  const res = {};
+  //normalize the sortedFilters
+  const sortedFilters = normalizeFilter(filter);
 
   const awaits = [];
   const awaitsMap = new Map();
   //this is the task that we need to do when we produce the output. doesn't handle await yet.
-  for (let [[parentPath, prop], [stateP, process]] of sortedFilters) {
+  for (let [parentPath, prop, stateP, process] of sortedFilters) {
     let val = stateP && getProp(state, stateP);//1. get the value.
     const obj = getPropFill(res, parentPath);
     val ??= obj[prop];
@@ -37,15 +42,14 @@ export function getValues(filter, state) {
       const list = awaitsMap.get(obj[prop]);
       if (list) {
         /*val =*/
-        Promise.all(list).then(_ => process(val)).then(v => /*v !== undefined && v !== null &&*/ (obj[prop] = v));
+        Promise.all(list).then(_ => process(val)).then(v => v !== undefined && /*v !== null &&*/ (obj[prop] = v));
         awaits.push(val);
         continue;
       }
       val = process(val);
     }
-    obj[prop] = val;
     if (val instanceof Promise) {
-      awaits.push(val), val.then(v => obj[prop] = v);
+      awaits.push(val), val.then(v => v!== undefined && (obj[prop] = v));
 
       //todo adding the promise under each parent
       for (let parent of getParents(res, parentPath)) {
@@ -54,7 +58,8 @@ export function getValues(filter, state) {
         list.push(val);
       }
 
-    }
+    }else
+      obj[prop] = val;
   }
   return awaits.length ? Promise.all(awaits).then(_ => res) : res;
 }
